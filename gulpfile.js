0bf24274953dc5413817e3036c538d3f1bfff055
David Jensen <david.lgj@gmail.com>
2015-08-13 11:34:25 +0200

    Migrated gulp tasks and protractor tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,5 @@
-var gulp = require('gulp'),
-  streamqueue = require('streamqueue'),
-  minifyHtml = require('gulp-minify-html'),
-  templateCache = require('gulp-angular-templatecache'),
-  concat = require('gulp-concat'),
-  rename = require('gulp-rename'),
-  uglify = require('gulp-uglify');
 
-gulp.task('build', function() {
-  var stream = streamqueue({objectMode: true});
-  stream.queue(
-    gulp.src('./src/**/*.html')
-    .pipe(minifyHtml({
-      empty: true,
-      spare: true,
-      quotes: true
-    }))
-    .pipe(templateCache({
-      module: 'schemaForm',
-      root: 'decorators/bootstrap/'
-    }))
-    );
-  stream.queue(gulp.src('./src/**/*.js'));
+//all of the tasks themselves are contained in the gulp/tasks directory,
+//which is accessed through gulp/index.js
 
-  stream.done()
-  .pipe(concat('bootstrap-decorator.js'))
-  .pipe(gulp.dest('./'))
-  .pipe(uglify())
-  .pipe(rename('bootstrap-decorator.min.js'))
-  .pipe(gulp.dest('./'));
-
-});
-
-
-gulp.task('watch', function() {
-  gulp.watch('./src/**/*', ['default']);
-});
-
-gulp.task('default', ['build']);
+require('./gulp');
