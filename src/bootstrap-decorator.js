c802f0b69e52470d1cee810957d3e3d7cee2e0dc
Chase Sterling <chase.sterling@gmail.com>
2015-09-28 19:00:22 -0400

    Put ng-if on the correct items in a formSelect

--- src/bootstrap-decorator.js
+++ src/bootstrap-decorator.js
@@ -30,7 +30,7 @@ function(decoratorsProvider, sfBuilderProvider, sfPathProvider) {
   };
 
   var formSelectItemsNgShow = function(args) {
-    var children = args.fieldFrag.querySelectorAll('[sf-field-transclude] [sf-field]');
+    var children = args.fieldFrag.querySelectorAll('[sf-field-transclude] .form-group');
 
     for (var i = 0; i < children.length; i++) {
       children[i].setAttribute('ng-if', 'selectedForm.value === ' + i);
