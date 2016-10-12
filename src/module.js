c63414e90004035dca797de70b52e986ed6a2793
Marcel <iamanthropic@gmail.com>
2016-10-13 01:08:41 +1100

    Clean up loader and compiler, remove more gulp

--- src/module.js
+++ src/module.js
@@ -1,4 +1,2 @@
 require('../node_modules/angular-schema-form/dist/schema-form.js');
-//require('./angular-schema-form-bootstrap-templates.js');
-//require('./type-parser.js');
 require('bootstrap-decorator.js');
